1bd364ee205e4220cc17c52bebe34475f52e9316
PhilosopherOfCode <148618956+PhilosopherOfCode@users.noreply.github.com>
2023-10-21 14:50:37 +0400

    Update SIGNGGLOVE (TRANSLATOR).py

--- SIGNGGLOVE (TRANSLATOR).py
+++ SIGNGGLOVE (TRANSLATOR).py
@@ -1,32 +1,68 @@
-import serial
-import time
-# Configure the serial connections
-arduino1_port = "COM4"  # Replace with the actual COM port for Arduino 1
-arduino2_port = "COM7"  # Replace with the actual COM port for Arduino 2
-baud_rate = 9600
+from PIL import Image
+from PIL import ImageTk
 
-arduino1_serial = serial.Serial(arduino1_port, baud_rate)
-arduino2_serial = serial.Serial(arduino2_port, baud_rate)
+import tkinter as tk
+from tkinter import ttk
 
-while True:
-    # Read data from Arduino 1
-    data1 = arduino1_serial.readline().decode().strip()
-    # Read data from Arduino 2
-    data2 = arduino2_serial.readline().decode().strip()
+import json
 
-    # Process data from Arduino 1
-    sensor_data1 = data1.split(",")
+#window
+window = tk.Tk()		
+window.title('SignoGlove')
+window.geometry('700x800')
+window.configure(bg='blue')
+
+background_image=tk.PhotoImage(file = "C:\\Users/Emmanuel\\Dropbox/My PC (DESKTOP-3AI9NQB)\\Downloads\\Signo Glove\\Textures\\OIG.png")
+background_label = tk.Label(window, image=background_image)
+background_label.place(x=0, y=0, relwidth=1, relheight=1)
+
+TTT_Button = tk.PhotoImage(file='C:\\Users\Emmanuel\\Dropbox\\My PC (DESKTOP-3AI9NQB)\\Downloads\\Signo Glove\\Textures\\TTS-removebg-preview_mini.png')
+Edit_audio_Btton = tk.PhotoImage(file="C:\\Users\\Emmanuel\\Dropbox\\My PC (DESKTOP-3AI9NQB)\\Downloads\\Signo Glove\\Textures\\AUDIO_Profile-removebg-preview_mini.png")
+
+
+
+def Edit_TTS_Profile():
+	Edit_audio.pack_forget()
+	TTS.pack_forget()
+	title_label.pack_forget()
+	scale.pack(pady=350)
+	title_label.place_forget()
+	
+	Back.place(x=260, y=5)
+ 
+
+def Back():
+    scale.pack_forget()
+    Back.place_forget()
+    title_label.place(x=260, y=5)
+    title_label.lift()
     
-    # Process data from Arduino 2
-    sensor_data2 = data2.split(",")
     
-    # Print or process the sensor data from both Arduino
-    print("Arduino 1 Sensor Data:", sensor_data1)
-    print("Arduino 2 Sensor Data:", sensor_data2)
+    Edit_audio.pack(pady = 50)
+    TTS.pack(pady = 20)
 
-    X1, Y1, Z1 , F1, F2, F3, F4, F5 = sensor_data1
-    X2, Y2, Z2 , F6, F7, F8, F9, F10 = sensor_data2
 
-    time.sleep(0.5)
+# Header
+title_label = tk.Label(master = window, text = 'SignoGlove', font = 'SegoeUI 24 bold italic')
+title_label.pack(side = 'top', anchor = 'n')
 
-    
+# Buttons
+
+#input_frame = tk.Frame(master = window)
+#input_frame.configure(bg = 'blue')
+Back = tk.Button(window,text = 'Back', command = Back )
+scale = tk.Scale(window,from_ = 0,to = 100, orient = 'horizontal', label = 'Volume', length = 200, width = 20, font = ('Consolas', 15 ) )
+Back.configure(height=5,width=20)
+Edit_audio = tk.Button(window,text = 'Edit audio profile', command = Edit_TTS_Profile, image= Edit_audio_Btton, compound = 'top', font= ('Comic Sans' , 20))
+Edit_audio.pack(pady = 50)
+
+TTS = tk.Button(window,text = 'Text to speech', image= TTT_Button, compound = 'top', font= ('Comic Sans' , 20))
+
+TTS.pack(pady = 20)
+
+#input_frame.pack()
+
+#if Edit_audio 
+
+#run
+window.mainloop()
